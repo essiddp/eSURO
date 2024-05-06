b572769a2b1e7f3a8b3c832914bcf9efb18cd1e0
Ian Lapuz <iannathaniellapuz@gmail.com>
2024-05-07 03:46:29 +0800

    added binomial expansion functionality for pascal's triangle

--- assets/js/pascal.js
+++ assets/js/pascal.js
@@ -1,9 +1,12 @@
 const button = document.getElementById('inputButton');
 const triangleContent = document.getElementById('triangle');
 
+let numCache = [];
+
 function solveTriangle(n) 
 {
   let result = [];
+  numCache = []; //Clear cache memory
   for (let line = 1; line <= n; line++) 
   {
     let numLine = [];
@@ -12,6 +15,10 @@ function solveTriangle(n)
     let C = 1;
     for (let i = 1; i <= line; i++) 
     {
+      if (line == n)
+      {
+        numCache.push(C);
+      }
       // The first value in a line is always 1
       numLine.push(C + "&nbsp&nbsp&nbsp&nbsp&nbsp");
       C = C * (line - i) / i;
@@ -21,10 +28,38 @@ function solveTriangle(n)
   return result.join("<br>");
 }
 
+function getExpansion(n)
+{
+  let terms = [];
+  let xval, yval;
+  for (let i = 0; i <= n; i++)
+  {
+    if (i == 0)
+    {
+      terms.push("x<sup>" + (n - 1) + "</sup>");
+    }
+    else if (i == n)
+    {
+      terms.push("y<sup>" + (n - 1) + "</sup>");
+    }
+    else
+    {
+      xval = (n - 1) - i;
+      yval = i;
+      if (xval > 0)
+      {
+        terms.push(numCache[i] + "x<sup>" + xval + "</sup>" + "y<sup>" + yval + "</sup>");
+      }
+    }
+  }
+  return terms.join(" + ");
+}
+
 function changeTriangle()
 {
   finalResult = solveTriangle(document.getElementById("numInput").value);
-  triangleContent.innerHTML = '<p>' + finalResult + '</p>';
+  expResult = getExpansion(document.getElementById("numInput").value);
+  triangleContent.innerHTML = '<p>' + finalResult + '<br><br>' + "(x + y)<sup>n-1</sup> " + "Expansion:<br>" +expResult +'</p>';
 }
 
 button.addEventListener('click', changeTriangle);
