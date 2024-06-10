413dec2509db815340329cf44287ec2407a87491
essiddp <jessireedp22@gmail.com>
2024-06-11 00:59:44 +0800

    added random button

--- renderer.js
+++ renderer.js
@@ -10,3 +10,24 @@
   }
   
 // });
+
+const urls = [
+  "in-app/jeopardy/indexJeopardy.html",
+  "in-app/pascal.html",
+  "in-app/wordhunt/wordhunt.html",
+  "in-app/wheel.html",
+  "in-app/bingo/indexBingo.html"
+];
+
+// Function to get a random URL from the array
+function getRandomUrl() {
+  const randomIndex = Math.floor(Math.random() * urls.length);
+  return urls[randomIndex];
+}
+
+// Event listener for the image click
+document.getElementById("random-link").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default anchor click behavior
+  const randomUrl = getRandomUrl();
+  window.location.href = randomUrl; // Redirect to the random URL
+});
