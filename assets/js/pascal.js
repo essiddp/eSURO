1fe664129d7262b7882236c667ee73de871b6953
essiddp <jessireedp22@gmail.com>
2024-06-11 05:33:00 +0800

    fixed pascal and jeopardy quesh ui

--- assets/js/pascal.js
+++ assets/js/pascal.js
@@ -1,54 +1,41 @@
 const button = document.getElementById('inputButton');
+const home = document.getElementById('homeButton');
 const triangleContent = document.getElementById('triangle');
 const form = document.getElementById('triangleForm');
 
 let numCache = [];
 
-function solveTriangle(n) 
-{
+function solveTriangle(n) {
   let result = [];
-  numCache = []; //Clear cache memory
-  for (let line = 1; line <= n; line++) 
-  {
+  numCache = []; // Clear cache memory
+  for (let line = 1; line <= n; line++) {
     let numLine = [];
-    
-    // used to represent C(line, i)
-    let C = 1;
-    for (let i = 1; i <= line; i++) 
-    {
-      if (line == n)
-      {
+    let C = 1; // used to represent C(line, i)
+    for (let i = 1; i <= line; i++) {
+      if (line == n) {
         numCache.push(C);
       }
       // The first value in a line is always 1
       numLine.push(C + "&nbsp&nbsp&nbsp&nbsp&nbsp");
       C = C * (line - i) / i;
     }
-      result.push(numLine.join(""));
+    result.push(numLine.join(""));
   }
   return result.join("<br>");
 }
 
-function getExpansion(n)
-{
+function getExpansion(n) {
   let terms = [];
   let xval, yval;
-  for (let i = 0; i <= n; i++)
-  {
-    if (i == 0)
-    {
+  for (let i = 0; i <= n; i++) {
+    if (i == 0) {
       terms.push("x<sup>" + (n - 1) + "</sup>");
-    }
-    else if (i == n)
-    {
+    } else if (i == n) {
       terms.push("y<sup>" + (n - 1) + "</sup>");
-    }
-    else
-    {
+    } else {
       xval = (n - 1) - i;
       yval = i;
-      if (xval > 0)
-      {
+      if (xval > 0) {
         terms.push(numCache[i] + "x<sup>" + xval + "</sup>" + "y<sup>" + yval + "</sup>");
       }
     }
@@ -71,4 +58,22 @@ form.addEventListener('submit', function(event) {
     const expResult = getExpansion(num);
     triangleContent.innerHTML = '<p>' + finalResult + '<br><br>' + "(x + y)<sup>n-1</sup> " + "Expansion:<br>" + expResult + '</p>';
   }
+});
+
+function redirectToHome(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  window.location.href = '../index.html'; // Redirect to home
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Add event listener to the home button
+  const homeButton = document.getElementById('homeButton');
+  homeButton.addEventListener('click', redirectToHome);
+
+  // Add event listener to prevent form submission on Enter key press within the form
+  form.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  });
 });
