8d67ae3786343b507e66381d2781490e0ccab33a
essiddp <jessireedp22@gmail.com>
2024-06-21 11:10:18 +0800

    fixed wheel incorrect option

--- assets/js/wheel.js
+++ assets/js/wheel.js
@@ -6,7 +6,7 @@
         r = Math.min(w, h) / 2,
         rotation = 0,
         oldrotation = 0,
-        picked = 100000,
+        picked = 1000000,
         oldpick = -1,
         color = d3.scale.ordinal().range([
             '#D4070F',
@@ -15,8 +15,8 @@
             '#01D93F',
             '#0069C9',
             '#59009F',
-        ]);
-        
+    ]);
+
     var originalData = [];
     var data = originalData.slice(); // Create a copy to preserve the original data
     var svg = d3.select('#chart')
@@ -61,54 +61,14 @@
             .text(function(d, i) { return data[i]; });
     }
 
-    // Initial creation of the wheel
-    updateWheel();
+        // Initial creation of the wheel
+        updateWheel();
 
-    container.on("click", spin);
-    
+        container.on("click", spin);
 
     function spin() {
         if (data.length === 0) {
-            return; // Exit the function if there are no inputs
-        }
-
-        // Store the input text content before disabling
-        var inputText = document.getElementById("inputText").value;
-        
-        container.on("click", null);
-    
-        var ps = 360 / data.length,
-            rng = Math.floor((Math.random() * 1440) + 360);
-    
-        // Calculate the new rotation angle based on current state
-        oldrotation = rotation;
-        rotation = (Math.round(rng / ps) * ps);
-        picked = Math.round(data.length - (rotation % 360) / ps);
-        picked = picked >= data.length ? (picked % data.length) : picked;
-        rotation += 3000 - Math.round(ps / 2);
-    
-        // Calculate the duration dynamically based on data length
-        var duration = 10800; // Adjust this factor as needed
-        
-        // Animate the rotation from oldrotation to new rotation
-        vis.transition()
-            .duration(duration)
-            .attrTween("transform", rotTween)
-            .each("end", function() {
-                // Restore the input text content
-                document.getElementById("inputText").value = inputText;
-                
-                oldpick = picked;
-                showPopup(data[picked]);
-                
-                // Update oldrotation to current rotation after spin completes
-                oldrotation = rotation;
-                
-                // Re-enable click event after spin completes
-                container.on("click", spin);
-            });
-
-        setTimeout(spinSound, 0);
+        return; // Exit the function if there are no inputs
     }
 
     function spinSound() {
@@ -118,64 +78,102 @@
         music.pause();
         setTimeout(replayMusic, 11100);
     }
-    
+
     function replayMusic() {
         const music = document.getElementById('music');
         music.play();
     }
-  
+
+    // Store the input text content before disabling
+    var inputText = document.getElementById("inputText").value;
+
+    container.on("click", null);
+
+    var ps = 360 / data.length,
+        rng = Math.floor((Math.random() * 1440) + 360);
+
+    // Calculate the new rotation angle based on current state
+    oldrotation = rotation;
+    rotation = (Math.round(rng / ps) * ps);
+    picked = Math.round(data.length - (rotation % 360) / ps);
+    picked = picked >= data.length ? (picked % data.length) : picked;
+    rotation += 1170 - Math.round(ps / 2);
+
+    // Calculate the duration dynamically based on data length
+    var duration =  10800;; // Adjust this factor as needed
+
+    // Animate the rotation from oldrotation to new rotation
+    vis.transition()
+        .duration(duration)
+        .attrTween("transform", rotTween)
+        .each("end", function() {
+            // Restore the input text content
+            document.getElementById("inputText").value = inputText;
+            
+            oldpick = picked;
+            showPopup(data[picked]);
+            
+            // Update oldrotation to current rotation after spin completes
+            oldrotation = rotation;
+            
+            // Re-enable click event after spin completes
+            container.on("click", spin);
+        });
+        setTimeout(spinSound, 0);
+    }
+
     svg.append("g")
-            .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
-            .append("path")
-            .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
-            .style({"fill":"#ffffff"});
-        //draw spin circle
-        container.append("circle")
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("r", 60)
-            .style({"fill":"#ffffff","cursor":"pointer"});
-        //spin text
-        container.append("text")
-            .attr("x", 0)
-            .attr("y", 15)
-            .attr("text-anchor", "middle")
-            .text("SPIN")
-            .style({"font-weight":"bold", "font-size":"30px", "font-color":"white", "cursor":"pointer"});
-        
-        
+        .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
+        .append("path")
+        .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
+        .style({"fill":"#ffffff"});
+    //draw spin circle
+    container.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 60)
+        .style({"fill":"#ffffff","cursor":"pointer"});
+    //spin text
+    container.append("text")
+        .attr("x", 0)
+        .attr("y", 15)
+        .attr("text-anchor", "middle")
+        .text("SPIN")
+        .style({"font-weight":"bold", "font-size":"30px", "font-color":"white", "cursor":"pointer"});
+
+
 
     function rotTween(to) {
-        var i = d3.interpolate(oldrotation % 360, rotation);
-        return function(t) {
-            return "rotate(" + i(t) + ")";
-        };
+    var i = d3.interpolate(oldrotation % 360, rotation);
+    return function(t) {
+        return "rotate(" + i(t) + ")";
+    };
     }
 
     // Define the submit button click event
     document.getElementById("submitBtn").addEventListener("click", function() {
-        // Get the input text area
-        var inputTextArea = document.getElementById("inputText");
-        // Get the value from the text area
-        var newInput = inputTextArea.value;
-        // Check if the new input is not empty
-        if (newInput !== '') {
-            // Split the input by lines
-            var newChoices = newInput.split('\n');
-            // Process the new choices
-            processInput(newChoices);
-        }
+    // Get the input text area
+    var inputTextArea = document.getElementById("inputText");
+    // Get the value from the text area
+    var newInput = inputTextArea.value;
+    // Check if the new input is not empty
+    if (newInput !== '') {
+        // Split the input by lines
+        var newChoices = newInput.split('\n');
+        // Process the new choices
+        processInput(newChoices);
+    }
     });
 
     function processInput(newChoices) {
-        // Clear the data array
-        data = [];
-        // Trim and add each input directly to the data
-        newChoices.forEach(function(d) {
-            var trimmed = d.trim();
-            if (trimmed !== '') {
-                data.push(trimmed);
-            }
+    // Clear the data array
+    data = [];
+    // Trim and add each input directly to the data
+    newChoices.forEach(function(d) {
+        var trimmed = d.trim();
+        if (trimmed !== '') {
+            data.push(trimmed);
+        }
         });
         // Remove existing slices
         vis.selectAll(".slice").remove();
@@ -221,3 +219,4 @@
         // Update the wheel
         updateWheel();
     });
+
